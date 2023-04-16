b1a007e7b613263a7c59e23e92eb136c22802196
cn <121707519+examinationfirst@users.noreply.github.com>
2023-04-16 09:57:27 +0800

    Create color.cmd
    在cmd.exe上运行并按住回车键

---
+++ color.cmd
@@ -0,0 +1,141 @@
+color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0
+color c0
+
+color d0
+
+color e0
+
+coloe f0color a0
+
+color b0
+
+color c0
+
+color d0
+
+color e0
+
+coloe f0
